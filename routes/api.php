afa8d38d7fa38823ff557447460d9926ca17a382
João Vinicius da Costa <joaovc@uninove.br>
2024-07-30 11:57:17 -0300

    Implements auth, login and tokens with sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,19 @@
 <?php
 
 use App\Http\Controllers\AlbumController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 Route::prefix('v1')->group(function(){
     Route::get('/album/search', [AlbumController::class, 'search']);
